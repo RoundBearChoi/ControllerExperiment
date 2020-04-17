c8389a5bd564c801c26311be8b5440a33ef3d121
RoundBearChoi <roundbeargames@gmail.com>
2020-04-18 03:02:57 +0900

    Torque Angle Difference

--- controllerexp/Assets/ControllerExperiment/AngleCalculator.cs
+++ controllerexp/Assets/ControllerExperiment/AngleCalculator.cs
@@ -9,10 +9,11 @@ namespace ControllerExperiment
         public static float GetAngle(float x, float z)
         {
             float value = (float)((Mathf.Atan2(x, z) / System.Math.PI) * 180f);
-            if (value < 0)
-            {
-                value += 360f;
-            }
+
+            //if (value < 0)
+            //{
+            //    value += 360f;
+            //}
 
             return value;
         }
--- controllerexp/Assets/ControllerExperiment/Player/CharacterController.cs
+++ controllerexp/Assets/ControllerExperiment/Player/CharacterController.cs
@@ -6,7 +6,10 @@ namespace ControllerExperiment
 {
     public class CharacterController : MonoBehaviour
     {
-        public float Angle;
+        public AnimationCurve TorqueMultiplier;
+
+        float Angle;
+        float AngleDifference;
         Rigidbody rbody;
         TargetAngle targetAngle;
         GameObject ShowTorque;
@@ -16,23 +19,30 @@ namespace ControllerExperiment
             rbody = this.gameObject.GetComponent<Rigidbody>();
             targetAngle = GameObject.FindObjectOfType<TargetAngle>();
             ShowTorque = GameObject.Find("Torque");
-
-            //rbody.AddTorque(new Vector3(0f, 10f, 0f));
         }
 
         private void FixedUpdate()
         {
-            
-
             Angle = AngleCalculator.GetAngle(this.transform.forward.x, this.transform.forward.z);
+            AngleDifference = (targetAngle.Angle - Angle);
 
-            float dif = (targetAngle.Angle - Angle);
-            Vector3 torque = (Vector3.up * dif);
+            if (Mathf.Abs(AngleDifference) > 180f)
+            {
+                if (AngleDifference < 0f)
+                {
+                    AngleDifference = (360f + AngleDifference);
+                }
+                else if (AngleDifference > 0f)
+                {
+                    AngleDifference = (360f - AngleDifference) * -1f;
+                }
+            }
+
+            Vector3 torque = (Vector3.up * AngleDifference);
             rbody.AddTorque(Vector3.up * -rbody.angularVelocity.y, ForceMode.VelocityChange);
             rbody.AddTorque(torque, ForceMode.VelocityChange);
 
             ShowTorque.transform.position = this.transform.position + torque;
-            //Debug.Log(dif);
         }
     }
 }
