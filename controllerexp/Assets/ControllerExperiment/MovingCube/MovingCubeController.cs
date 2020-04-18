92eb0d86d31d669e892db65d5c7d5227d2921920
RoundBearChoi <roundbeargames@gmail.com>
2020-04-18 20:39:24 +0900

    moving cube test prefab

---
+++ controllerexp/Assets/ControllerExperiment/MovingCube/MovingCubeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ControllerExperiment
+{
+    public class MovingCubeController : MonoBehaviour
+    {
+        Rigidbody rbody;
+        public float MoveSpeed;
+
+        private void Start()
+        {
+            rbody = this.GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            rbody.AddForce(Vector3.forward * -rbody.velocity.z, ForceMode.VelocityChange);
+            rbody.AddForce(Vector3.right * -rbody.velocity.x, ForceMode.VelocityChange);
+
+            rbody.AddForce(-Vector3.forward * MoveSpeed, ForceMode.VelocityChange);
+        }
+    }
+}
