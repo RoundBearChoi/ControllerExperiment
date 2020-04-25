55d201d2ce63c6f30f6f1a8c715891981add5024
RoundBearChoi <roundbeargames@gmail.com>
2020-04-25 14:51:41 +0900

    amy ragdoll setup

---
+++ controllerexp/Assets/ControllerExperiment/Ragdoll/Editor/RagdollControllerEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace ControllerExperiment.Ragdoll
+{
+    [CustomEditor(typeof(RagdollController))]
+    public class RagdollControllerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            RagdollController r = (RagdollController)target;
+
+            if (GUILayout.Button("Get Ragdoll Parts"))
+            {
+                r.GetRagdollParts();
+            }
+
+            if (GUILayout.Button("Get Character Joints"))
+            {
+                r.GetCharacterJoints();
+            }
+        }
+    }
+}
---
+++ controllerexp/Assets/ControllerExperiment/Ragdoll/RagdollController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ControllerExperiment.Ragdoll
+{
+    public class RagdollController : MonoBehaviour
+    {
+        public List<GameObject> RagdollParts = new List<GameObject>();
+        public List<CharacterJoint> CharacterJoints = new List<CharacterJoint>();
+
+        private void Start()
+        {
+            RagdollParts.Clear();
+            GetRagdollParts();
+        }
+
+        public void GetRagdollParts()
+        {
+            foreach (Transform child in this.transform)
+            {
+                Collider[] colliders = this.gameObject.GetComponentsInChildren<Collider>();
+
+                foreach (Collider c in colliders)
+                {
+                    // do not include self
+                    if (c.gameObject != this.gameObject)
+                    {
+                        if (!RagdollParts.Contains(c.gameObject))
+                        {
+                            RagdollParts.Add(c.gameObject);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void GetCharacterJoints()
+        {
+            CharacterJoints.Clear();
+
+            CharacterJoint[] joints = this.gameObject.GetComponentsInChildren<CharacterJoint>();
+
+            foreach (CharacterJoint j in joints)
+            {
+                if (!CharacterJoints.Contains(j))
+                {
+                    CharacterJoints.Add(j);
+                }
+            }
+        }
+    }
+}
