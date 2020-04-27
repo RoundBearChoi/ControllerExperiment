8c9e3d985a2585abfac6050854fcad5c25a0118e
RoundBearChoi <roundbeargames@gmail.com>
2020-04-28 00:39:19 +0900

    layers added controler + amy ragdoll

---
+++ controllerexp/Assets/ControllerExperiment/LayerAdder/LayerAdder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ControllerExperiment
+{
+    public enum RB_Layers
+    {
+        PostProcessing,
+        RAGDOLL,
+        GROUND,
+    }
+
+    public class LayerAdder : MonoBehaviour
+    {
+
+    }
+}
