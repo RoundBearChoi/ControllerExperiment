20e3bbf86c56745c3541f6d63c97e0d17d31629f
RoundBearChoi <roundbeargames@gmail.com>
2020-04-29 04:22:09 +0900

    namespace (player & ragdoll divided)

--- controllerexp/Assets/ControllerExperiment/SubComponents/Player SubComponents/Keys/SetPlayer.cs
+++ controllerexp/Assets/ControllerExperiment/SubComponents/Player SubComponents/Keys/SetPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ControllerExperiment.SubComponents
+namespace ControllerExperiment.SubComponents.Player
 {
     public class SetPlayer : SubComponentKey
     {
