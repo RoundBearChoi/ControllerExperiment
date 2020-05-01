dbc35d6997d84bc21aa766b7d8178befae8460c9
RoundBearChoi <roundbeargames@gmail.com>
2020-05-01 17:58:50 +0900

    follow vs no follow

--- controllerexp/Assets/ControllerExperiment/StateMachine/Ragdoll States/Ragdoll Transform/InstantFollowController.cs
+++ controllerexp/Assets/ControllerExperiment/StateMachine/Ragdoll States/Ragdoll Transform/InstantFollowController.cs
@@ -11,6 +11,13 @@ namespace ControllerExperiment.States.Ragdoll
         {
             subComponentProcessor.SetEntity(SetRagdoll.INSTANT_ROTATE_ENTITY);
             subComponentProcessor.SetEntity(SetRagdoll.INSTANT_MOVE_ENTITY);
+
+            RagdollTransformState t = (RagdollTransformState)subComponentProcessor.GetInt(RagdollInt.RAGDOLL_TRANSFORM_STATE);
+        
+            if (t == RagdollTransformState.NO_FOLLOW)
+            {
+                stateProcessor.TransitionTo(typeof(NoFollow));
+            }
         }
     }
 }
--- controllerexp/Assets/ControllerExperiment/StateMachine/Ragdoll States/Ragdoll Transform/NoFollow.cs
+++ controllerexp/Assets/ControllerExperiment/StateMachine/Ragdoll States/Ragdoll Transform/NoFollow.cs
@@ -10,6 +10,13 @@ namespace ControllerExperiment.States.Ragdoll
         public override void ProcStateFixedUpdate()
         {
             subComponentProcessor.SetEntity(SetRagdoll.INSTANT_ROTATE_ENTITY);
+
+            RagdollTransformState t = (RagdollTransformState)subComponentProcessor.GetInt(RagdollInt.RAGDOLL_TRANSFORM_STATE);
+
+            if (t == RagdollTransformState.INSTANT_FOLLOW)
+            {
+                stateProcessor.TransitionTo(typeof(InstantFollowController));
+            }
         }
     }
 }
