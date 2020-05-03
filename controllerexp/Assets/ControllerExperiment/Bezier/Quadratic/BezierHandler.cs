40f0b8f0e2ee29300cc79f71c4f9558c36b66c79
RoundBearChoi <roundbeargames@gmail.com>
2020-05-03 19:05:28 +0900

    setting up many

---
+++ controllerexp/Assets/ControllerExperiment/Bezier/Quadratic/BezierHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ControllerExperiment
+{
+    public class BezierHandler : MonoBehaviour
+    {
+        public float time;
+        public float timeScale;
+        public GameObject BezierPrefab;
+
+        private IEnumerator Start()
+        {
+            time = 0f;
+
+            InstantiateBezier();
+
+            while(true)
+            {
+                time += (Time.deltaTime * timeScale);
+
+                if (time >= 1f)
+                {
+                    time = 0f;
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+        void InstantiateBezier()
+        {
+            GameObject obj = Instantiate(BezierPrefab);
+            obj.transform.position = Vector3.zero;
+
+            obj.GetComponent<OptimizedBezier>().owner = this;
+        }
+    }
+}
---
+++ controllerexp/Assets/ControllerExperiment/Bezier/Quadratic/OptimizedBezier.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+
+namespace ControllerExperiment
+{
+    public class OptimizedBezier : MonoBehaviour
+    {
+        [SerializeField] GameObject Cube;
+		[SerializeField] List<GameObject> Checkpoints = new List<GameObject>();
+		public BezierHandler owner;
+
+		QuadraticBezierJob job;
+		JobHandle jobHandle;
+
+		public void MoveCube()
+        {
+			NativeArray<Vector3> result = new NativeArray<Vector3>(1, Allocator.TempJob);
+
+			job = new QuadraticBezierJob()
+			{
+				p0 = Checkpoints[0].transform.position,
+				p1 = Checkpoints[1].transform.position,
+				p2 = Checkpoints[2].transform.position,
+				time = owner.time,
+				useInefficientCode = false,
+				positionArray = result
+			};
+
+			jobHandle = job.Schedule();
+			jobHandle.Complete();
+			Cube.transform.position = result[0];
+			result.Dispose();
+		}
+
+		private void Update()
+		{
+			MoveCube();
+		}
+	}
+}
