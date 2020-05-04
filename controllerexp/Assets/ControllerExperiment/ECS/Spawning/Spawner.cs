86c7fea444b64060286d5d4088be00266923fbf2
RoundBearChoi <roundbeargames@gmail.com>
2020-05-04 18:06:31 +0900

    spawning options

--- controllerexp/Assets/ControllerExperiment/ECS/Spawning/Spawner.cs
+++ controllerexp/Assets/ControllerExperiment/ECS/Spawning/Spawner.cs
@@ -10,29 +10,98 @@ namespace ControllerExperiment
 {
     public class Spawner : MonoBehaviour
     {
+        public enum SpawnType
+        {
+            TRADITIONAL,
+            PURE_ECS,
+            CONVERSION,
+        }
+
+        [SerializeField] SpawnType spawnType;
+        [SerializeField] int zCount;
+        [SerializeField] int yCount;
+        [SerializeField] float spacing;
+
+        [Header("Pure ECS")]
         [SerializeField] Mesh CubeMesh;
         [SerializeField] Material BlueMaterial;
 
+        [Header("Conversion")]
+        [SerializeField] GameObject CubePrefab;
+
+        World world;
+        EntityManager entityManager;
+        GameObjectConversionSettings conversionSettings;
+        int entitycount;
+
         private void Start()
         {
-            CreateEntity();
+            entitycount = 0;
+            SpawnMany(zCount, yCount, spacing);
         }
 
-        void CreateEntity()
+        void SpawnMany(int gridZ, int gridY, float spacing)
         {
-            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            if (spawnType == SpawnType.TRADITIONAL)
+            {
+                for (int y = 0; y < gridY; y++)
+                {
+                    for (int z = 0; z < gridZ; z++)
+                    {
+                        GameObject obj = GameObject.Instantiate(CubePrefab,
+                            (Vector3.forward * z * spacing) + (Vector3.up * y * spacing),
+                            Quaternion.identity);
+                    }
+                }
+            }
+
+            else if (spawnType == SpawnType.PURE_ECS)
+            {
+                entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+                for (int y = 0; y < gridY; y++)
+                {
+                    for (int z = 0; z < gridZ; z++)
+                    {
+                        CreateEntity(new float3(0f, y * spacing, z * spacing));
+                        entitycount++;
+                    }
+                }
+            }
+            else if (spawnType == SpawnType.CONVERSION)
+            {
+                world = World.DefaultGameObjectInjectionWorld;
+                entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+                for (int y = 0; y < gridY; y++)
+                {
+                    for (int z = 0; z < gridZ; z++)
+                    {
+                        conversionSettings = GameObjectConversionSettings.FromWorld(world, null);
+                        Entity entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(CubePrefab, conversionSettings);
+
+                        CreateConvertedEntity(entity, new float3(0f, y * spacing, z * spacing));
+                        entitycount++;
+                    }
+                }
+            }
+        }
+
+        void CreateEntity(float3 position)
+        {
             Entity entity = entityManager.CreateEntity();
-            entityManager.SetName(entity, "My Spawned Entity");
+
+            entityManager.SetName(entity, "My Spawned Entity " + entitycount);
 
             // adding position values
             entityManager.AddComponentData(entity, new Translation {
-                Value = new float3(0f, 0f, 0f) 
+                Value = position
             });
 
             // adding rotation values
             entityManager.AddComponentData(entity, new Rotation
             {
-                Value = Quaternion.Euler(new Vector3(0f, 45f, 0f))
+                Value = Quaternion.Euler(new Vector3(0f, 0f, 0f))
             });
 
             // adding render values
@@ -42,6 +111,24 @@ namespace ControllerExperiment
             });
 
             // adding render values (used by hybrid renderer)
+            entityManager.AddComponentData(entity, new LocalToWorld { });
+        }
+
+        void CreateConvertedEntity(Entity entity, float3 position)
+        {
+            entityManager.Instantiate(entity);
+            entityManager.SetName(entity, "My Converted Entity " + entitycount);
+
+            entityManager.AddComponentData(entity, new Translation
+            {
+                Value = position
+            });
+
+            entityManager.AddComponentData(entity, new Rotation
+            {
+                Value = Quaternion.Euler(new Vector3(0f, 45f, 0f))
+            });
+
             entityManager.AddComponentData(entity, new LocalToWorld { });
         }
     }
