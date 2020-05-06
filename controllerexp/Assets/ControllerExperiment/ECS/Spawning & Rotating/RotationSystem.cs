b952a15c0fbf11af1cf1904f18cbab88ea5b57a3
RoundBearChoi <roundbeargames@gmail.com>
2020-05-06 14:10:46 +0900

    single thread vs multithread

--- controllerexp/Assets/ControllerExperiment/ECS/Spawning & Rotating/RotationSystem.cs
+++ controllerexp/Assets/ControllerExperiment/ECS/Spawning & Rotating/RotationSystem.cs
@@ -7,20 +7,31 @@ using Unity.Collections;
 
 namespace ControllerExperiment
 {
+    // multithread
     public class RotationSystem : SystemBase
     {
         protected override void OnUpdate()
         {
             float deltaTime = Time.DeltaTime;
-
+    
             Entities.ForEach((ref Rotation rotation) =>
             {
-                // get angle on y-axis
                 quaternion yRot = quaternion.RotateY(180f * Mathf.Deg2Rad * deltaTime);
-
-                // matrix multiplication
                 rotation.Value = math.mul(rotation.Value, yRot);
             }).Schedule();
         }
     }
+
+    // single thread
+    //public class RotationSystem: ComponentSystem
+    //{
+    //    protected override void OnUpdate()
+    //    {
+    //        Entities.ForEach((ref Rotation rotation) =>
+    //        {
+    //            quaternion yRot = quaternion.RotateY(180f * Mathf.Deg2Rad * Time.DeltaTime);
+    //            rotation.Value = math.mul(rotation.Value, yRot);
+    //        });
+    //    }
+    //}
 }
